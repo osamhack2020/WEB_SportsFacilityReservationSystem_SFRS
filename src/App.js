4f7ee076f1227132df6d9ad253044fa9af5fc323
younganJung1115 <youngan.jung@gmail.com>
2020-10-19 00:20:42 +0900

    vscodespace 접근 오류로 수동으로 올립니다.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import Edit from "./fontawesome/edit";
 import User from "./fontawesome/user";
 import Login from "./fontawesome/login";
 import Logout from "./fontawesome/logout";
-import IndexPage from "./indexPage";
 import PrivateRoute from "./privateRoute";
 import SignUp from "./signUp";
 import SignIn from "./signIn";
 import MyInfoPage from "./myInfoPage";
+import AddFacility from "./addFacility";
+import AddCamp from "./addCamp";
 import CampList from "./campList";
 import Checkout from "./reservation";
 import Reservation from "./reservation";
 import app from "./firebase";
-import { AuthProvider } from "./auth";
+import { AuthProvider, AuthContext } from "./auth";
+import Divider from "@material-ui/core/Divider";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import BackgroundImage from "./backgroundImage";
+import GridContainer from "./gridContainer.js";
+import GridItem from "./gridItem.js";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import {
   BrowserRouter as Router,
   Switch,
@@ -22,15 +30,84 @@ import {
   NavLink,
 } from "react-router-dom";
 
-const App = () => {
-  const [isLogined, setIsLogined] = React.useState(false);
+const useStyles = makeStyles(() => ({
+  container: {
+    zIndex: "12",
+    color: "#FFFFFF",
+    paddingRight: "15px",
+    paddingLeft: "15px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    width: "100%",
+    "@media (min-width: 576px)": {
+      maxWidth: "540px",
+    },
+    "@media (min-width: 768px)": {
+      maxWidth: "720px",
+    },
+    "@media (min-width: 992px)": {
+      maxWidth: "960px",
+    },
+    "@media (min-width: 1200px)": {
+      maxWidth: "1140px",
+    },
+  },
+  title: {
+    margin: "1.75rem 0 0.875rem",
+    fontWeight: "700",
+    fontFamily: `"Jua", sans-serif`,
+    display: "inline-block",
+    position: "relative",
+    marginTop: "30px",
+    minHeight: "32px",
+    color: "#FFFFFF",
+    textDecoration: "none",
+  },
+}));
 
-  React.useEffect(() => {
-    app.auth().onAuthStateChanged((user) => {
-      if (user) setIsLogined(true);
-      else setIsLogined(false);
-    });
-  });
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5",
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "center",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center",
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    "&:hover": {
+      backgroundColor: "#0f4c8133",
+    },
+    "&:active": {
+      backgroundColor: "#0f4c8133",
+    },
+  },
+}))(MenuItem);
+
+const App = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const classes = useStyles();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AuthProvider>
@@ -45,7 +122,6 @@ const App = () => {
                 &nbsp;체육시설 예약체계
               </NavLink>
             </div>
-
             <ul className="navbar__menu">
               <li>
                 <NavLink to="/reservation">예약신청</NavLink>
@@ -59,37 +135,123 @@ const App = () => {
               <li>
                 <NavLink to="/checkout">마이페이지</NavLink>
               </li>
+              <AuthContext.Consumer>
+                {({ currentUser, userInfo }) => {
+                  if (currentUser && userInfo.admin === true) {
+                    return (
+                      <div>
+                        <li>
+                          <NavLink to="/addCamp" onClick={handleClick}>
+                            관리자페이지
+                          </NavLink>
+                        </li>
+                        <StyledMenu
+                          id="customized-menu"
+                          anchorEl={anchorEl}
+                          keepMounted
+                          open={Boolean(anchorEl)}
+                          onClose={handleClose}
+                        >
+                          <StyledMenuItem
+                            id="fistSmallMenu"
+                            style={{
+                              padding: 0,
+                            }}
+                          >
+                            <NavLink
+                              to="/addCamp"
+                              style={{
+                                padding: "6px 16px",
+                                display: "inline-block",
+                              }}
+                            >
+                              부대 관리
+                            </NavLink>
+                          </StyledMenuItem>
+                          <StyledMenuItem
+                            style={{
+                              padding: 0,
+                            }}
+                          >
+                            <NavLink
+                              to="/addFacility"
+                              style={{
+                                padding: "6px 16px",
+                                display: "inline-block",
+                              }}
+                            >
+                              체육시설 관리
+                            </NavLink>
+                          </StyledMenuItem>
+                        </StyledMenu>
+                      </div>
+                    );
+                  }
+                }}
+              </AuthContext.Consumer>
             </ul>
 
             <ul className="navbar__icons">
               <li>
                 <User />
-                {isLogined ? (
-                  <NavLink to="/myInfoPage">&nbsp;내정보</NavLink>
-                ) : (
-                  <NavLink to="/signUp">&nbsp;회원가입</NavLink>
-                )}
+                <AuthContext.Consumer>
+                  {({ currentUser }) =>
+                    currentUser ? (
+                      <NavLink to="/myInfoPage">&nbsp;내정보</NavLink>
+                    ) : (
+                      <NavLink to="/signUp">&nbsp;회원가입</NavLink>
+                    )
+                  }
+                </AuthContext.Consumer>
               </li>
-              {isLogined ? (
-                <li>
-                  <Logout />
-                  <a href="#logout" onClick={() => app.auth().signOut()}>
-                    &nbsp;로그아웃
-                  </a>
-                </li>
-              ) : (
-                <li>
-                  <Login />
-                  <NavLink to="/login">&nbsp;로그인</NavLink>
-                </li>
-              )}
+
+              <AuthContext.Consumer>
+                {({ currentUser }) =>
+                  currentUser ? (
+                    <li>
+                      <Logout />
+                      <NavLink
+                        to="/"
+                        onClick={() => {
+                          app.auth().signOut();
+                        }}
+                      >
+                        &nbsp;로그아웃
+                      </NavLink>
+                    </li>
+                  ) : (
+                    <li>
+                      <Login />
+                      <NavLink to="/login">&nbsp;로그인</NavLink>
+                    </li>
+                  )
+                }
+              </AuthContext.Consumer>
             </ul>
             <Bars />
           </nav>
         </div>
+        <Divider />
 
         <Switch>
-          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/">
+            <BackgroundImage filter image={require("./assets/bg.jpg")}>
+              <div className={classes.container}>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <h1 className={classes.title}>
+                      국군장병들의 건강한 체육활동을 위하여
+                    </h1>
+                    <h4>
+                      간부 병 상관없이 누구나 확률적으로 즐길수있는 공정한
+                      체육시설예약체계 각 부대에서 있는 체육대회에도 많은 관심
+                      부탁드립니다.
+                    </h4>
+                  </GridItem>
+                </GridContainer>
+              </div>
+            </BackgroundImage>
+          </Route>
 
           <PrivateRoute path="/reservation" component={Reservation} />
 
@@ -98,6 +260,14 @@ const App = () => {
           <Route path="/login" component={SignIn} />
 
           <Route path="/myInfoPage" component={MyInfoPage} />
+
+          <Route
+            path="/addFacility"
+            component={AddFacility}
+            // render={() => <AddFacility user={currentUser} />}
+          />
+
+          <Route path="/addCamp" component={AddCamp} />
 
           <Route path="/signUp" component={SignUp} />
 
--- src/auth.js
+++ src/auth.js
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from "react";
 import app from "./firebase";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = (props) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const [userInfo, setUserInfo] = useState(false);
   const [pending, setPending] = useState(true);
 
   useEffect(() => {
     app.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user)
-      setPending(false)
+      if (user) {
+        app
+          .firestore()
+          .collection("users")
+          .where("uid", "==", user.uid)
+          .get()
+          .then((snapshot) => {
+            snapshot.forEach((doc) => {
+              setUserInfo(doc.data());
+            });
+          });
+      }
+      setCurrentUser(user);
+      setPending(false);
     });
   }, []);
 
   if (pending) {
-    return <>Loading...</>
+    return <CircularProgress />;
   }
 
   return (
     <AuthContext.Provider
       value={{
-        currentUser
+        currentUser,
+        userInfo,
       }}
     >
       {props.children}
