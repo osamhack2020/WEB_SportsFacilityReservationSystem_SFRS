e529560e2aca98120a532af8b4c828f1f1cd05ea
younganJung1115 <youngan.jung@gmail.com>
2020-10-25 07:16:30 +0000

    get max schedules

--- src/cloudFunctionTestPage.js
+++ src/cloudFunctionTestPage.js
@@ -1,10 +1,10 @@
 import React from "react";
 import app from "./firebase";
 import moment from "moment";
+import Button from "@material-ui/core/Button";
 
-const IndexPage = () => {
-  const db = app.firestore();
-
+const Test = () => {
+  // const db = app.firestore();
   // db.collection("camp")
   //   .get()
   //   .then((snapshot) => {
@@ -47,33 +47,154 @@ const IndexPage = () => {
   //     });
   //   });
 
-  const startTime = moment()
-    .startOf("day")
-    .subtract(1, "days")
-    .toDate()
-    .getTime();
-  const endTime = moment().endOf("day").subtract(1, "days").toDate().getTime();
+  const startTime = moment().startOf("day").add(1, "days").toDate().getTime();
+  const endTime = moment().endOf("day").add(1, "days").toDate().getTime();
+  const [array, setArray] = React.useState([]);
+  const [maxScheduleCount, setMaxScheduleCount] = React.useState(0);
+  const [maxScheduleCandidate, setMaxScheduleCandidate] = React.useState([]);
+  const [uniqueMaxSchedule, setUniqueMaxSchedule] = React.useState([]);
 
-  db.collection("camp")
-    .doc("10함대")
-    .collection("facility")
-    .doc("야구장")
-    .collection("reservation")
-    .where("start", ">=", new Date(startTime))
-    .where("start", "<=", new Date(endTime))
-    .get()
-    .then((snapshot) => {
-      snapshot.forEach((doc) => {
-        console.log(doc.data().start.toDate());
-        console.log(doc.data().end.toDate());
+  React.useEffect(() => {
+    app
+      .firestore()
+      .collection("camp")
+      .doc("1함대")
+      .collection("facility")
+      .doc("축구장")
+      .collection("reservation")
+      .where("end", ">=", new Date(startTime))
+      .where("end", "<=", new Date(endTime))
+      .orderBy("end")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          setArray((oldArray) => [
+            ...oldArray,
+            {
+              title: doc.data().title,
+              documentId: doc.id,
+              start: doc.data().start.seconds,
+              end: doc.data().end.seconds,
+            },
+          ]);
+
+          // console.log(doc.id);
+          // console.log(doc.data().title);
+          // console.log(doc.data().start.toDate());
+          // console.log(doc.data().end.toDate());
+        });
       });
-    });
+  }, [startTime, endTime]);
+
+  const clickHandle = () => {
+    console.log(array);
+  };
+
+  const maxSchedule = () => {
+    let endPoint = array[0].end;
+    let temp = [array[0].title];
+
+    for (let i = 0; i < array.length; i++) {
+      if (endPoint <= array[i].start) {
+        endPoint = array[i].end;
+        temp.push(array[i].title);
+      }
+    }
+
+    setMaxScheduleCandidate((oldArray) => [...oldArray, temp]);
+    setMaxScheduleCount(temp.length);
+  };
+
+  const untilNothingLeft = async (arr, line) => {
+    if (arr.length === 1) {
+      line.push(array[arr[0]].title);
+      if (line.length === maxScheduleCount) {
+        for (let k = 0; k < maxScheduleCandidate.length; k++) {
+          if (
+            !line.every(
+              (value, index) => value === maxScheduleCandidate[k][index]
+            )
+          ) {
+            await setMaxScheduleCandidate((oldArray) => [...oldArray, line]);
+          }
+        }
+      }
+      return;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      let tempArray = line.slice();
+      tempArray.push(array[arr[i]].title);
+      untilNothingLeft(returnChildSchedule(arr[i]), tempArray);
+    }
+  };
+
+  const returnChildSchedule = (index) => {
+    let temp = [];
+    for (let i = index + 1; i < array.length; i++) {
+      if (array[i].start >= array[index].end) temp.push(i);
+    }
+    return temp;
+  };
+
+  const allPossibleSchedule = () => {
+    for (let i = 0; i < array.length - maxScheduleCount + 1; i++) {
+      let temp = [];
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i].end <= array[j].start) temp.push(j);
+        untilNothingLeft(temp, [array[i].title]);
+      }
+    }
+  };
+
+  const printMaxPossibleSchedule = () => {
+    let arr = maxScheduleCandidate.slice();
+    let uniqueArr = [];
+    for (let k = 0; k < arr.length; k++) {
+      uniqueArr.push(arr[k]);
+      for (let i = k + 1; i < arr.length; i++) {
+        if (arr[k].every((value, index) => value === arr[i][index])) {
+          arr.splice(i, 1);
+        }
+      }
+    }
+
+    let realUniqueArray = [];
+    for (let k = 0; k < uniqueArr.length; k++) {
+      realUniqueArray.push(uniqueArr[k]);
+      for (let i = k + 1; i < uniqueArr.length; i++) {
+        if (
+          uniqueArr[k].every((value, index) => value === uniqueArr[i][index])
+        ) {
+          uniqueArr.splice(i, 1);
+        }
+      }
+    }
+
+    setUniqueMaxSchedule(realUniqueArray);
+  };
 
   return (
     <div>
       <h2>This is index page~</h2>
+      <Button onClick={clickHandle} variant="contained" color="primary">
+        array 만들기
+      </Button>{" "}
+      <Button onClick={maxSchedule} variant="contained" color="primary">
+        최대 가능 스케줄
+      </Button>
+      <Button onClick={allPossibleSchedule} variant="contained" color="primary">
+        test
+      </Button>
+      <Button
+        onClick={printMaxPossibleSchedule}
+        variant="contained"
+        color="primary"
+      >
+        가능한 경우 출력
+      </Button>
     </div>
   );
 };
 
-export default IndexPage;
+export default Test;
